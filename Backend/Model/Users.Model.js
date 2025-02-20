fe82ad1e5aaadb43ea769b5f57dab8de344a3598
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-02-20 07:57:47 +0530

    Users backend setup done ✅

--- Backend/Model/Users.Model.js
+++ Backend/Model/Users.Model.js
@@ -1,28 +1,28 @@
-// // Backend/Model/Users.Model.js
-// // practice/Backend/Model/Users.Model.js
+// Backend/Model/Users.Model.js
+// practice/Backend/Model/Users.Model.js
 
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
-// const usersSchema = mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     surname: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-
-
-// })
+const usersSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
 
 
-// export const Users = mongoose.model("Users", usersSchema) 
+})
+
+
+export const Users = mongoose.model("Users", usersSchema) 
--- Backend/Routes/UsersRouter.js
+++ Backend/Routes/UsersRouter.js
@@ -1,14 +1,14 @@
-// // practice/Backend/Routes/UsersRouter.js
-// import express from 'express'
-// import { getUsers, getUserByEmail, addUser } from '../Controller/UsersController.js'
+// practice/Backend/Routes/UsersRouter.js
+import express from 'express'
+import { getUsers, getUserByEmail, addUser } from '../Controller/UsersController.js'
 
-// const UsersRouter = express.Router()
-
-
-// UsersRouter.get("/", getUsers)
-// UsersRouter.get("/email/:emailId", getUserByEmail); // Fix parameter case
-// UsersRouter.post("/", addUser)
+const UsersRouter = express.Router()
 
 
+UsersRouter.get("/", getUsers)
+UsersRouter.get("/email/:emailId", getUserByEmail); // Fix parameter case
+UsersRouter.post("/", addUser)
 
-// export default UsersRouter
+
+
+export default UsersRouter
