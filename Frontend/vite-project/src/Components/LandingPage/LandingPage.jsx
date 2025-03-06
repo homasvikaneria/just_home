242f89cdf1976ee07173b8fbaaf9cdb84e7d11ae
Homasvi Kaneria <homasvi.kaneria.cg@gmail.com>
2025-03-07 00:13:28 +0530

    feat: improved UI and styling for Landing Page & Listings

--- Frontend/vite-project/src/Components/LandingPage/LandingPage.jsx
+++ Frontend/vite-project/src/Components/LandingPage/LandingPage.jsx
@@ -3,8 +3,7 @@ import React from "react";
 import { Link  , useNavigate } from "react-router-dom";
 import Mainnavbar from "../Mainnav/Mainnavbar"
 import './LandingPage.css'
-import Newsletter from "../Newsletter/Newsletter";
-import Footer from "../Footer/Footer";
+
 
 
 
@@ -18,8 +17,8 @@ const LandingPage = () => {
 
             <div className="lp-1">
                 <div><img className="lp-bgc" src="https://res.cloudinary.com/dmfjcttu9/image/upload/v1740116320/lmrzswktnauo3vjyxm1k.png" alt="" /></div>
-                <div class="text-container">
-                    The <span class="highlight">#1</span> site real estate <br /> professionals trust*
+                <div className="text-container">
+                    The <span className="highlight">#1</span> site real estate <br /> professionals trust*
                 </div>
                 <div className="sub-text-container">
                     From as low as $10 per day with limited time offer discounts. <br />
